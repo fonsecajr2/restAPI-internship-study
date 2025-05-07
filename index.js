c75435691285ee7acdb48249af3db3b5daa4e835
fonsecajr2 <sheltonfonsecad@gmail.com>
2025-05-07 05:50:01 +0530

    comments added in index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const users = require('./models/userModel.js')
 
+//routes declaration
 const userRoutes = require("./routes/userRoutes.js")
 const productRoutes = require("./routes/productRoutes")
 
 app.use(express.json())
 
+//using different routes
 app.use('/users', userRoutes)
 app.use('/products', productRoutes)
 
-
+//app initialization
 app.listen(port, () => {
     console.log("running on port: ", port)
 })
